3f4e892588455007f3fc724a6066da017606b143
AshesBen <106581422+AshesBen@users.noreply.github.com>
2022-06-07 10:44:48 +0800

    Update detect.py

--- detection/detect.py
+++ detection/detect.py
@@ -228,7 +228,7 @@ def parse_opt():
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--visualize', action='store_true', default=False,help='visualize features')
     parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='/home/zk203/zxd/yolov5-master/first_paper_result/new/poloss/detect', help='save results to project/name')
+    parser.add_argument('--project', default=ROOT/'runs/detect', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--line-thickness', default=2, type=int, help='bounding box thickness (pixels)')
