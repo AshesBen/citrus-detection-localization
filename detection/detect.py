2c33724ca91b94cd6ba9723ec9235e467caa6d12
AshesBen <106581422+AshesBen@users.noreply.github.com>
2022-06-07 10:33:33 +0800

    Update detect.py

--- detection/detect.py
+++ detection/detect.py
@@ -210,9 +210,9 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='/home/zk203/zxd/yolov5-master/first_paper_result/new/poloss/train/pw2s001aa025/exp/weights/last.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default= '/home/zk203/zxd/yolov5-master/data/971_test_data/all/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/citrus_second/ori_4855.yaml', help='dataset.yaml path')
+    parser.add_argument('--weights', nargs='+', type=str, default='', help='model path(s)')
+    parser.add_argument('--source', type=str, default= '', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/citrus.yaml', help='dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.5, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.3, help='NMS IoU threshold')
